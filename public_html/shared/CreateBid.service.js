8a690aa02329bdca7514db34dd9cb29f4cd3b090
zkysar <zach.kysar@gmail.com>
2016-04-02 01:20:03 -0700

    Removed clients ability to choose who they were bidding as by linking their socket on the server side with their userid

--- public_html/shared/CreateBid.service.js
+++ public_html/shared/CreateBid.service.js
@@ -1,26 +1,21 @@
 myApp.service("CreateBidService", function(mySocket){
 	service = this;
-	this.uid = "";
+
 	this.bidAmount = 0;
 	this.auctionID = "";
 	this.bidID = "";
 
-	this.createNewBid = function(uid,bidAmount,auctionID){
+	this.createNewBid = function(bidAmount,auctionID){
 		callback = function(data){
 			service.bidID = data._id;
 		}
 		mySocket.emit("createNewBid",{
-			uid: uid,
 			bidAmount: bidAmount,
 			auctionID: auctionID
 		});
 		mySocket.on("createNewBid",callback);
-		service.uid = uid;
 		service.bidAmount = bidAmount;
 		service.auctionID = auctionID;
-	}
-	this.getUId = function(){
-		return service.uid;
 	}
 	this.getBidAmount = function(){
 		return service.bidAmount;
