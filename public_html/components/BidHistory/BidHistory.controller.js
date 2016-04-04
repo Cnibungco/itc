62ce29b3db4d5e5ef376333921457d75200c6f62
Camille Nibungco <camille.nibungco@gmail.com>
2016-04-03 17:40:36 -0700

    hooked up cards to home

--- public_html/components/BidHistory/BidHistory.controller.js
+++ public_html/components/BidHistory/BidHistory.controller.js
@@ -2,7 +2,7 @@ myApp.controller('BidHistoryController', ['$scope', function($scope) {
 
    $scope.title = "BidHistory";
    $scope.text = 'Generic Service Title';
-   $scope.data = "data"
+   $scope.data = "data";
     
 }
                                          ]);
--- public_html/components/ServiceCard/ServiceCard.controller.js
+++ public_html/components/ServiceCard/ServiceCard.controller.js
@@ -6,6 +6,6 @@ myApp.component('card', {
   templateUrl: 'components/ServiceCard/ServiceCard.view.html',
   controller: ServiceCardController,
   bindings: {
-    text: '='
+    auction: '='
   }
 });
