d30dcc943344ff9b54e0ba43ac665f2a53585fe8
zkysar <zach.kysar@gmail.com>
2016-04-04 21:56:35 -0700

    Removed testing logs

--- public_html/shared/AuctionListener.service.js
+++ public_html/shared/AuctionListener.service.js
@@ -3,8 +3,6 @@ myApp.service("AuctionListenerService", function(mySocket, AuctionDetailsService
 	this.callback = function(){}; 
 	this.listen = function(auctionID){
 		service.callback = function(data){
-			console.log(data);
-			console.log(AuctionDetailsService.getResult());
 			AuctionDetailsService.getResult().bidHistory.push(data);
 		}
 		mySocket.emit("startAuctionListener",auctionID)
