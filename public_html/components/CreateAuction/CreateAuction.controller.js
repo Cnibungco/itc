58e4e3cc426f1f19cf7c2d283dea40ab5b3c73d5
zkysar <zach.kysar@gmail.com>
2016-04-08 00:31:26 -0700

    Fixing more conflicts

--- public_html/components/CreateAuction/CreateAuction.controller.js
+++ public_html/components/CreateAuction/CreateAuction.controller.js
@@ -9,30 +9,5 @@ myApp.controller('CreateAuctionController', ['$scope', 'CreateAuctionService', '
 	}
 	$scope.getAuction = CreateAuctionService.getAuction;
 
-    function uploadImage(imageBase64, callback){
-        uploadToImgur(imageBase64)
-            .then(function successCallback(response) {
-                console.log(response);
-                var imgLink = response.data.data.link;
-                callback(imgLink);
-            }, function errorCallback(response) {
-                console.log("Error");
-                console.log(response)
-            });
-    }
-
-    function uploadToImgur(imageBase64) {
-        return $http({
-            method: 'POST',
-            url: 'https://api.imgur.com/3/image',
-            headers: {
-                Authorization: "Client-ID cc68d31a7733d0c", // THIS might work
-                Accept: "application/json"
-            },
-            data: {
-                image: imageBase64.replace(/.*,/, ''),
-                type: 'base64'
-            }
-        })
-    }
+    
 }]);
--- public_html/shared/CreateAuction.service.js
+++ public_html/shared/CreateAuction.service.js
@@ -18,4 +18,30 @@ myApp.service("CreateAuctionService", function(mySocket, $state){
 	this.getAuction = function(){
 		return service.auction;
 	}
+	var uploadImage = function(imageBase64, callback){
+		uploadToImgur(imageBase64)
+			.then(function successCallback(response) {
+				console.log(response);
+				var imgLink = response.data.data.link;
+				callback(imgLink);
+			}, function errorCallback(response) {
+				console.log("Error");
+				console.log(response)
+			});
+	}
+
+	var uploadToImgur = function (imageBase64) {
+		return $http({
+			method: 'POST',
+			url: 'https://api.imgur.com/3/image',
+			headers: {
+				Authorization: "Client-ID cc68d31a7733d0c", // THIS might work
+				Accept: "application/json"
+			},
+			data: {
+				image: imageBase64.replace(/.*,/, ''),
+				type: 'base64'
+			}
+		})
+	}
 })
