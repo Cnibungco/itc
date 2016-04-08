88bab9d0c40c4a278105d9187a0e2f2e8f525825
Isaac Siegel <siegelhorn@gmail.com>
2016-04-08 06:42:17 -0700

    changed firebase ref

--- public_html/shared/OAuth.factory.js
+++ public_html/shared/OAuth.factory.js
@@ -1,5 +1,5 @@
 myApp.factory('Auth', function($firebaseAuth) {
-    var endPoint = "https://scorching-fire-629.firebaseio.com" ;
+    var endPoint = "https://itcsoftcom2016.firebaseio.com" ;
     var usersRef = new Firebase(endPoint);
     return $firebaseAuth(usersRef);
   });
